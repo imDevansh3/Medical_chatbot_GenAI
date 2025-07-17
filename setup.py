b2b9eba6510274077e3ce26006d8546eba857268
Devansh Thakkar <devansh3@Devanshs-MacBook-Pro.local>
2025-07-17 16:08:44 +0530

    project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = 'Gen AI project',
+    version= '0.0.0',
+    author='Devansh Thakkar',
+    author_email='Tdevansh45@gmail.com',
+    packages = find_packages(),
+    install_rquires = []
+)
