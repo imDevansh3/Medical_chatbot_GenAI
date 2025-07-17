dcbcbbdbb37729d247137ee9358618c2600bfc8c
Devansh Thakkar <devansh3@Devanshs-MacBook-Pro.local>
2025-07-17 19:12:18 +0530

    final

---
+++ src/prompt.py
@@ -0,0 +1,12 @@
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question."
+    "If the answer is not in the context, say exactly: 'I don't know.'\n"
+    "Do not use prior knowledge."
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
